e3e583fd7cbab8b9a0e175f73f915b9af6050bfc
cedou ramsey <RAMSEYC3>
2024-10-07 14:43:55 +0200

    new code

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ListBlogs from './ListBlogs';
-
 
 function Home() {
   const [blogs, setblogs] = useState([
@@ -9,9 +8,18 @@ function Home() {
     { title: "Welcome party", body: "Lorem ipsum...", author: "Ramsey", id: 2 },
     { title: "Web dev top tips ", body: "Lorem ipsum...", author: "Mario", id: 3 }
   ])
+
+  const handleDelete = (id) => {
+      const newBlogs = blogs.filter()
+  }
+
+  useEffect(() => {
+    console.log("use effect ran ")
+  });
+
   return (
     <div className='Home'>
-      <ListBlogs blogs={blogs} title={"All blogs"} />
+      <ListBlogs blogs={blogs} title={"All blogs"} handleDelete={handleDelete} />
       <ListBlogs blogs={blogs.filter((blog) => blog.author === 'Mario')} title={"Mario's blog"} />
     </div > //using the filter method to filter out all the blogs that are not written by mario 
   )
--- src/ListBlogs.js
+++ src/ListBlogs.js
@@ -1,20 +1,18 @@
 import React from 'react'
+import './css/ListBlogs.css'
 
-function ListBlogs(props) {
+function ListBlogs(props) { //I used a normal function not a arrow function 
     const blogs = props.blogs;
     const title = props.title;
 
     return (
         <div className='blog-list'>
             <h2>{title}</h2>
-            {blogs.map((blog) => (//sturdy this line of code map method bwing used to display information //
-                <div className="blog-preview" style={{
-                    padding: "10px 16px",
-                    margin: "20px 0",
-                    borderBottom: "1px solid #fafafa",
-                }} key={blog.id}>
+            {blogs.map((blog) => (//sturdy this line of code map method bwing used to display information
+                <div className="blog-preview" key={blog.id}>
                     <h2 style={{ color: " #f1356d", }}>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button className='delete-blog' onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))
             }
